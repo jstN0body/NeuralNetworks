a704824db57f6cd62959230b7d16ca20a9ff0081
jstN0body <raoboys12@gmail.com>
2022-02-05 23:46:11 -0500

    yeeee

--- src/com/github/jstN0body/neuralNetworks/training/ScalaTrainingSet.scala
+++ src/com/github/jstN0body/neuralNetworks/training/ScalaTrainingSet.scala
@@ -1,6 +1,6 @@
 package com.github.jstN0body.neuralNetworks.training
 
 class ScalaTrainingSet(private val in: Array[Double], private val out: Array[Double]) {
-    def getInput(): Array[Double] in
-    def getOutput(): Array[Double] out
+    def getInput: Array[Double] = in
+    def getOutput: Array[Double] = out
 }
---
+++ src/com/github/jstN0body/neuralNetworks/training/Test.scala
@@ -0,0 +1,5 @@
+package com.github.jstN0body.neuralNetworks.training
+
+object Test {
+  val set = new ScalaTrainingSet(Array(2, 0.24, 3), Array(0.2, 1.3, 42))
+}
